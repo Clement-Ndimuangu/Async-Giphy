ef6e5e31ff34ae1c2b5f9b94a34267d66d9f462e
Clement Ndimuangu <clement.ndimuangu@verizon.com>
2024-09-16 14:27:22 -0500

    done with the problem

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+// console.log(process.env.API_KEY)
+
+async function getImage(query){
+    try {
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+        const res = await fetch(url)
+        const data = await res.json()   
+        let index = Math.floor(Math.random() * 25)
+        console.log(data.data[index].url);
+        const result = []
+        for(let item of data.data){
+            result.push(item)
+        }  
+        return result;     
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+(async()=>{
+    await getImage('dog')
+})();
